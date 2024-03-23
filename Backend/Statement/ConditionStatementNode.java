000f7a2b0d9c3f7e39dacfd8ca4c128c792b2e66
patthaveekarn <patthaveekarn2544@gmail.com>
2024-03-24 01:59:57 +0700

    update statement,paser,token

--- Backend/Statement/ConditionStatementNode.java
+++ Backend/Statement/ConditionStatementNode.java
@@ -1,11 +1,13 @@
 package Backend.Statement;
 
+import Backend.GameProcess.Game;
+
 public class ConditionStatementNode {
     private boolean condition;
-    private ExecuteNode trueStatement;
-    private ExecuteNode falseStatement;
+    private Node.ExecuteNode trueStatement;
+    private Node.ExecuteNode falseStatement;
 
-    public ConditionStatementNode(boolean condition, ExecuteNode trueStatement, ExecuteNode falseStatement) {
+    public ConditionStatementNode(boolean condition, Node.ExecuteNode trueStatement, Node.ExecuteNode falseStatement) {
         this.condition = condition;
         this.trueStatement = trueStatement;
         this.falseStatement = falseStatement;
--- Backend/Statement/IfStatementNode.java
+++ Backend/Statement/IfStatementNode.java
@@ -1,31 +1,10 @@
 package Backend.Statement;
 
-public class IfStatementNode extends ConditionStatementNode {
+import Backend.GameProcess.Game;
 
-    // Constructor ที่รับพารามิเตอร์เป็นเงื่อนไข (ExpressionNode) และคำสั่งที่เป็นจริงและเท็จ (ExecuteNode)
-    public IfStatementNode(ExpressionNode conditionNode, ExecuteNode trueStatement, ExecuteNode falseStatement) {
+public class IfStatementNode extends ConditionStatementNode{
+    public IfStatementNode(Node.ExpressionNode conditionNode, Node.ExecuteNode trueStatement, Node.ExecuteNode falseStatement) {
         super(conditionNode, trueStatement, falseStatement);
-    }
 
-    // เมธอด execute เพื่อทำการประมวลผลคำสั่ง if-else
-    @Override
-    public boolean execute(Game bindings) {
-        // ตรวจสอบเงื่อนไข
-        boolean condition = conditionNode.evaluate(bindings);
-
-        // ถ้าเงื่อนไขเป็นจริง
-        if (condition) {
-            // ประมวลผลคำสั่งที่เป็นจริง
-            trueStatement.execute(bindings);
-        } else {
-            // ถ้าไม่เป็นจริง ประมวลผลคำสั่งที่เท็จ (หากมี)
-            if (falseStatement != null) {
-                falseStatement.execute(bindings);
-            }
-        }
-
-        // คำสั่ง IfStatementNode ไม่ได้ส่งค่าคืน ให้คืนค่าเป็น true เพื่อแสดงว่าประมวลผลเสร็จสิ้น
-        return true;
     }
 }
-

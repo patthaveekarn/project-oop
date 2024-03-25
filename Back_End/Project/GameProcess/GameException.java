cb96323f3e68e35309c122854d3f9260de21566b
patthaveekarn <patthaveekarn2544@gmail.com>
2024-03-25 09:21:41 +0700

    update statement

---
+++ Back_End/Project/GameProcess/GameException.java
@@ -0,0 +1,25 @@
+package Back_End.Project.GameProcess;
+
+import Back_End.Project.Statement.NodeException;
+
+public class GameException extends RuntimeException {
+    public static class NotImplemented extends NodeException {
+        public NotImplemented() {
+            StackTraceElement[] stacktrace = Thread.currentThread().getStackTrace();
+            StackTraceElement e = stacktrace[2];
+            String className = e.getClassName();
+            String methodName = e.getMethodName();
+            throw new NotImplemented(String.format("%s.%s not implemented", className, methodName));
+        }
+
+        private NotImplemented(String m) {
+            super(m);
+        }
+    }
+    public static class GameEnded extends NodeException {
+        public GameEnded() {
+            super("game already ended");
+        }
+
+    }
+}
